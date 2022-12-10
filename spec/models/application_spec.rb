0150f6d1b1a960be9aa83e2ddc747b20b31215eb
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-12-09 21:22:08 -0700

    fix: Add end that was missed in rebase

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -58,6 +58,7 @@ RSpec.describe Application, type: :model do
     it { should validate_presence_of(:state) }
     it { should validate_presence_of(:zip) }
     it { should validate_presence_of(:description) }
+  end
 
   describe '#can_add_pets?' do
     it 'is true if application is not rejected or accepted' do
