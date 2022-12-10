88ceba1fdfb75fbf4a4621025aa05d01268a83ad
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-12-10 12:54:29 -0700

    fix: Remove | from menu and clean up show page HTML

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationPetsController < ApplicationController
   def new
-    # look for a way to pass in 
+    # look for a better way to pass in app_id param
     ApplicationPet.create!(application_id: params[:application_id], pet_id: params[:id])
     redirect_to "/applications/#{params[:application_id]}"
   end
