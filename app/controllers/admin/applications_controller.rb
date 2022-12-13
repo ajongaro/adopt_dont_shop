e3108578b6e7802cd7ae41079257df6bbaea24c8
Kelsie Porter <krporter22@gmail.com>
2022-12-12 17:53:38 -0700

    feat: Add #pending? to show.html.erb

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,6 +1,5 @@
 class Admin::ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
-    # @application_pets = ApplicationPets.where(application_id: params[:id], pet_id: _______ )
   end
 end
