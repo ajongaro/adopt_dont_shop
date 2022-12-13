7655ab548c6ad6050ba04b8c5607f0b405d0372d
Kelsie Porter <krporter22@gmail.com>
2022-12-13 13:53:30 -0700

    Remove comments

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationPetsController < ApplicationController
   def new
-    # Is there a way to use strong params for non-matching params?
     ApplicationPet.create!(application_id: params[:application_id], pet_id: params[:id])
     redirect_to "/applications/#{params[:application_id]}"
   end
