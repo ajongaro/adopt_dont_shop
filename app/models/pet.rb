f8f396bacd28b68819f72f0245e51c1279f7c058
Kelsie Porter <krporter22@gmail.com>
2022-12-12 17:52:47 -0700

    Create #pending? and associated test

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -5,11 +5,20 @@ class Pet < ApplicationRecord
   has_many :application_pets
   has_many :applications, through: :application_pets
 
+  def self.adoptable
+    where(adoptable: true)
+  end
+
   def shelter_name
     shelter.name
   end
 
-  def self.adoptable
-    where(adoptable: true)
+  def pending?(application_id)
+    # starting with the AR association of application_pets (from above)
+    # so that we can return a single application_pet record.
+    app_pet_record = application_pets.find_by(
+      application_id: application_id
+    )
+    app_pet_record.status == "Pending"
   end
 end
