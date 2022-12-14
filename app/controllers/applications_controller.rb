8cb40a00d2b4f89922c7df3e9b29f3828df78b20
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-12-13 20:40:00 -0700

    fix: Reformat some code

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationsController < ApplicationController
 
   def new
-    
   end
 
   def show
@@ -13,9 +12,7 @@ class ApplicationsController < ApplicationController
   end
 
   def create 
-    application = Application.new(
-      permitted_params.merge({status: "In Progress"})
-    )
+    application = Application.new(permitted_params.merge({status: "In Progress"}))
 
     if application.save
       redirect_to "/applications/#{application.id}"
@@ -27,16 +24,13 @@ class ApplicationsController < ApplicationController
 
   def update 
     application = Application.find(params[:id])
-    application.update(
-      description: params[:description], 
-      status: "Pending"
-    )
+    application.update(description: params[:description], status: "Pending")
 
     redirect_to "/applications/#{application.id}"
   end
 
   private
-
+  
   def permitted_params
     params.permit(
       :human_name,
